e23039f235281d2bc6aeccd5d1c17193f0eec57b
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-05-01 10:22:42 +0400

    navbar complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar";
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,6 +2,10 @@ import { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
 
 const NavBar = () => {
   const [activeLink, setActiveLink] = useState<string>("home");
@@ -20,23 +24,25 @@ const NavBar = () => {
     return () => window.removeEventListener("scroll", onScroll);
   }, []);
 
-  const onUpdateActiveLink = (value: string) => {};
+  const onUpdateActiveLink = (value: string) => {
+    setActiveLink(value);
+  };
 
   return (
-    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+    <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
       <Container>
         <Navbar.Brand href="#home">
-          <img src={""} alt="Logo" />
+          <img src={logo} alt="Logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
           <span className="navbar-toggler-icon"></span>
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="ms-auto">
             <Nav.Link
               href="#home"
               className={
-                activeLink === "home" ? "active-navbar-link" : "navbar-link"
+                activeLink === "home" ? "active navbar-link" : "navbar-link"
               }
               onClick={() => onUpdateActiveLink("home")}
             >
@@ -45,7 +51,7 @@ const NavBar = () => {
             <Nav.Link
               href="#skills"
               className={
-                activeLink === "skills" ? "active-navbar-link" : "navbar-link"
+                activeLink === "skills" ? "active navbar-link" : "navbar-link"
               }
               onClick={() => onUpdateActiveLink("skills")}
             >
@@ -54,7 +60,7 @@ const NavBar = () => {
             <Nav.Link
               href="#projects"
               className={
-                activeLink === "projects" ? "active-navbar-link" : "navbar-link"
+                activeLink === "projects" ? "active navbar-link" : "navbar-link"
               }
               onClick={() => onUpdateActiveLink("projects")}
             >
@@ -63,16 +69,19 @@ const NavBar = () => {
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
-              <a href="#" className="">
-                <img src="" alt="" />
+              <a
+                href="https://www.linkedin.com/in/giorgi-makharadze-a8100125b/"
+                className=""
+              >
+                <img src={navIcon1} alt="" />
               </a>
 
               <a href="#" className="">
-                <img src="" alt="" />
+                <img src={navIcon2} alt="" />
               </a>
 
               <a href="#" className="">
-                <img src="" alt="" />
+                <img src={navIcon3} alt="" />
               </a>
             </div>
             <button className="vvd" onClick={() => console.log("form")}>
