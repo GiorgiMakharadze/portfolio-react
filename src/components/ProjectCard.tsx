1514969a25c90e587a93af0885d101a90ca1b058
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2023-05-01 15:54:59 +0400

    projects

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,25 @@
+import { Col } from "react-bootstrap";
+import { IProjectCardProps } from "../../types/projectCardProps";
+
+const ProjectCard = ({
+  title,
+  description,
+  imageUrl,
+  url,
+}: IProjectCardProps) => {
+  return (
+    <Col size={12} sm={6} md={4}>
+      <div className="proj-imgbx">
+        <img src={imageUrl} alt={title} />
+        <div className="proj-txtx">
+          <a href={url} className="link">
+            {title}
+          </a>
+          <br />
+          <span>{description}</span>
+        </div>
+      </div>
+    </Col>
+  );
+};
+export default ProjectCard;
---
+++ types/projectCardProps.ts
@@ -0,0 +1,6 @@
+export interface IProjectCardProps {
+  title?: string;
+  description?: string;
+  imageUrl?: string;
+  url?: string;
+}
